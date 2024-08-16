496c33d13fb46d8eea2c483252b2d1fcdba25548
Shayan Shafquat <shafquat.shayan@gmail.com>
2024-08-16 06:25:46 +0100

    observation var dec with time

--- src/uncertainty_directed_exporation.py
+++ src/uncertainty_directed_exporation.py
@@ -2,15 +2,19 @@ import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.stats import pearsonr
+from scipy.stats import pearsonr, norm
+from sklearn.linear_model import LinearRegression
+
 from utils import prepare_avg_rewards
+
+
 
 class Agent:
     def __init__(self, decay_rate_means, decay_rate_stds, observation_var=0.00001):
         self.decay_rate_means = decay_rate_means
         self.decay_rate_stds = decay_rate_stds
         self.observation_var = observation_var
-        self.beliefs = {(i+1, env): {'mean': decay_rate_means[i], 'std': decay_rate_stds[i]}
+        self.beliefs = {(i+1, env): {'mean': decay_rate_means[i], 'std': decay_rate_stds[i], 'count': 0}
                         for i in range(len(decay_rate_means)) for env in [1, 2]}
         self.mean_history = {(i+1, env): [] for i in range(len(decay_rate_means)) for env in [1, 2]}
         self.std_history = {(i+1, env): [] for i in range(len(decay_rate_means)) for env in [1, 2]}
@@ -23,23 +27,28 @@ class Agent:
         belief = self.beliefs[(patch_type, env)]
         prior_mean = belief['mean']
         prior_var = belief['std'] ** 2
+        belief['count'] += 1
+        count = belief['count']
+
         if self.observation_var > 0:
-            # Weighted update using observation variance
-            observation_var = self.observation_var
-
-            posterior_mean = (prior_var * observed_decay_rate + observation_var * prior_mean) / (prior_var + observation_var)
-            posterior_var = (prior_var * observation_var) / (prior_var + observation_var)
-
-            self.beliefs[(patch_type, env)]['mean'] = posterior_mean
-            self.beliefs[(patch_type, env)]['std'] = np.sqrt(posterior_var)
+            # Adjust the observation variance to decrease with more observations
+            adjusted_observation_var = self.observation_var / count
+            
+            # Bayesian update for the mean and variance
+            posterior_mean = (prior_var * observed_decay_rate + adjusted_observation_var * prior_mean) / (prior_var + adjusted_observation_var)
+            posterior_var = (prior_var * adjusted_observation_var) / (prior_var + adjusted_observation_var)
         else:
-            # Direct update without observation variance
-            self.beliefs[(patch_type, env)]['mean'] = (observed_decay_rate + prior_mean)/2
-            # Optionally adjust the standard deviation or keep it constant
-            self.beliefs[(patch_type, env)]['std'] = prior_var / (prior_var + 1)
+            # Simple averaging for the mean if observation variance is zero
+            posterior_mean = (prior_mean * (count - 1) + observed_decay_rate) / count
+            posterior_var = prior_var * (count - 1) / count  # Decrease the uncertainty by the number of observations
 
-        self.mean_history[(patch_type, env)].append(self.beliefs[(patch_type, env)]['mean'])
-        self.std_history[(patch_type, env)].append(self.beliefs[(patch_type, env)]['std'])
+        # Update the Agent's belief with the new mean and reduced variance
+        self.beliefs[(patch_type, env)]['mean'] = posterior_mean
+        self.beliefs[(patch_type, env)]['std'] = np.sqrt(posterior_var)
+
+        # Log the updated belief
+        self.mean_history[(patch_type, env)].append(posterior_mean)
+        self.std_history[(patch_type, env)].append(np.sqrt(posterior_var))
 
     def decide_leave(self, reward, average_reward):
         return reward < average_reward
@@ -68,14 +77,17 @@ class Patch:
             return reward
         else:
             return self.initial_yield
-        
+
 class Simulation:
-    def __init__(self, decay_rate_means, decay_rate_stds, avg_rewards, observation_var=0.01):
+    def __init__(self, decay_rate_means, decay_rate_stds, avg_rewards, observation_var=0.01, random_seed=None):
         self.decay_rate_means = decay_rate_means
         self.decay_rate_stds = decay_rate_stds
         self.avg_rewards = avg_rewards
         self.observation_var = observation_var
         self.patch_types = self.initialize_env()
+
+        if random_seed is not None:
+            np.random.seed(random_seed)
 
     def initialize_env(self):
         patch_types = [
@@ -102,22 +114,20 @@ class Simulation:
                 patch_info['decay_rate_mean']
             )
             patch.start_harvesting()
-            # # Sample decay rate based on the updated belief
+            
+            # Sample decay rate based on the updated belief
             sampled_decay_rate = agent.sample_decay_rate(trial['patch'], env)
-            # patch.decay_rate = sampled_decay_rate
+            patch.decay_rate = sampled_decay_rate  # Apply the sampled decay rate to the patch
+
             for t in range(1, n_max + 1):
                 reward = patch.get_reward()
 
-                if agent.decide_leave(reward, avg_reward):
+                if agent.decide_leave(reward, avg_reward) or t>=n_max:
                     observed_decay_rate = -np.log(reward / patch_info['initial_yield']) / t if t > 0 else sampled_decay_rate
                     agent.update_belief(trial['patch'], env, observed_decay_rate)
                     patch_sequence.append(trial['patch'])
                     leave_times.append(t)
                     break
-                # else:
-                #     # Update the belief even if the agent decides to stay
-                #     observed_decay_rate = -np.log(reward / patch_info['initial_yield']) / t if t > 0 else sampled_decay_rate
-                #     agent.update_belief(trial['patch'], env, observed_decay_rate)
 
         return patch_sequence, leave_times
 
@@ -146,3 +156,193 @@ class Simulation:
             df_trials['simulated_leaveT'] = simulated_leave_times
             df_trials['simulated_dmLeave'] = df_trials['simulated_leaveT'] - df_trials['meanLT']
             return df_trials
+
+# class Agent:
+#     def __init__(self, decay_rate_means, decay_rate_stds, observation_var=0.00001):
+#         self.decay_rate_means = decay_rate_means
+#         self.decay_rate_stds = decay_rate_stds
+#         self.observation_var = observation_var
+#         self.beliefs = {(i+1, env): {'mean': decay_rate_means[i], 'std': decay_rate_stds[i], 'count': 0}
+#                         for i in range(len(decay_rate_means)) for env in [1, 2]}
+#         self.mean_history = {(i+1, env): [] for i in range(len(decay_rate_means)) for env in [1, 2]}
+#         self.std_history = {(i+1, env): [] for i in range(len(decay_rate_means)) for env in [1, 2]}
+#         self.model_weights = {'exponential': 1/3, 'linear': 1/3, 'quadratic': 1/3}  # Initialize model weights
+
+#     def sample_decay_rate(self, patch_type, env):
+#         belief = self.beliefs[(patch_type, env)]
+#         return np.random.normal(belief['mean'], belief['std'])
+
+#     def update_belief(self, patch_type, env, observed_data):
+#         belief = self.beliefs[(patch_type, env)]
+#         prior_mean = belief['mean']
+#         prior_var = belief['std'] ** 2
+#         belief['count'] += 1
+#         count = belief['count']
+
+#         # Update model weights using Bayesian inference
+#         model_likelihoods = self.calculate_model_likelihoods(observed_data)
+#         self.update_model_weights(model_likelihoods)
+
+#         # Calculate a weighted average of the decay rate based on model weights
+#         posterior_mean = 0
+#         for model, weight in self.model_weights.items():
+#             if model == 'exponential':
+#                 posterior_mean += weight * self.estimate_exponential_decay(observed_data)
+#             elif model == 'linear':
+#                 posterior_mean += weight * self.estimate_linear_decay(observed_data)
+#             elif model == 'quadratic':
+#                 posterior_mean += weight * self.estimate_quadratic_decay(observed_data)
+
+#         # Bayesian update for variance, if observation variance > 0
+#         if self.observation_var > 0:
+#             adjusted_observation_var = self.observation_var / count
+#             posterior_var = (prior_var * adjusted_observation_var) / (prior_var + adjusted_observation_var)
+#         else:
+#             posterior_var = prior_var * (count - 1) / count  # Decrease the uncertainty by the number of observations
+
+#         # Update the Agent's belief with the new mean and reduced variance
+#         self.beliefs[(patch_type, env)]['mean'] = posterior_mean
+#         self.beliefs[(patch_type, env)]['std'] = np.sqrt(posterior_var)
+#         self.mean_history[(patch_type, env)].append(posterior_mean)
+#         self.std_history[(patch_type, env)].append(np.sqrt(posterior_var))
+
+#     def calculate_model_likelihoods(self, observed_data):
+#         likelihoods = {}
+#         for model in self.model_weights.keys():
+#             if model == 'exponential':
+#                 likelihoods['exponential'] = self.exponential_likelihood(observed_data)
+#             elif model == 'linear':
+#                 likelihoods['linear'] = self.linear_likelihood(observed_data)
+#             elif model == 'quadratic':
+#                 likelihoods['quadratic'] = self.quadratic_likelihood(observed_data)
+#         return likelihoods
+
+#     def update_model_weights(self, likelihoods):
+#         total_likelihood = sum(likelihoods.values())
+#         for model in self.model_weights.keys():
+#             self.model_weights[model] *= likelihoods[model] / total_likelihood
+
+#     def estimate_exponential_decay(self, observed_data):
+#         return -np.log(observed_data[-1] / observed_data[0]) / len(observed_data)
+
+#     def estimate_linear_decay(self, observed_data):
+#         X = np.arange(len(observed_data)).reshape(-1, 1)
+#         y = np.array(observed_data)
+#         model = LinearRegression().fit(X, y)
+#         return -model.coef_[0]
+
+#     def estimate_quadratic_decay(self, observed_data):
+#         X = np.arange(len(observed_data)).reshape(-1, 1)
+#         y = np.array(observed_data)
+#         X_quadratic = np.hstack((X, X**2))
+#         model = LinearRegression().fit(X_quadratic, y)
+#         return -model.coef_[1]  # Decay rate from the quadratic term
+
+#     def exponential_likelihood(self, observed_data):
+#         decay_rate = self.estimate_exponential_decay(observed_data)
+#         predicted = [observed_data[0] * np.exp(-decay_rate * t) for t in range(len(observed_data))]
+#         return np.prod(norm.pdf(observed_data, loc=predicted))
+
+#     def linear_likelihood(self, observed_data):
+#         decay_rate = self.estimate_linear_decay(observed_data)
+#         predicted = [observed_data[0] - decay_rate * t for t in range(len(observed_data))]
+#         return np.prod(norm.pdf(observed_data, loc=predicted))
+
+#     def quadratic_likelihood(self, observed_data):
+#         decay_rate = self.estimate_quadratic_decay(observed_data)
+#         predicted = [observed_data[0] - decay_rate * t**2 for t in range(len(observed_data))]
+#         return np.prod(norm.pdf(observed_data, loc=predicted))
+
+#     def decide_leave(self, reward, average_reward):
+#         return reward < average_reward
+
+# class Simulation:
+#     def __init__(self, decay_rate_means, decay_rate_stds, avg_rewards, observation_var=0.01, batch_size=10):
+#         self.decay_rate_means = decay_rate_means
+#         self.decay_rate_stds = decay_rate_stds
+#         self.avg_rewards = avg_rewards
+#         self.observation_var = observation_var
+#         self.batch_size = batch_size
+#         self.patch_types = self.initialize_env()
+
+#     def initialize_env(self):
+#         patch_types = [
+#             {'type': 1, 'initial_yield': 32.5, 'decay_rate_mean': 0.075},
+#             {'type': 2, 'initial_yield': 45, 'decay_rate_mean': 0.075},
+#             {'type': 3, 'initial_yield': 57.5, 'decay_rate_mean': 0.075}
+#         ]
+#         return patch_types
+
+#     def get_patch_info(self, patch_type):
+#         for patch in self.patch_types:
+#             if patch['type'] == patch_type:
+#                 return patch
+#         raise ValueError("Patch type not found")
+
+#     def simulate_subject(self, subject_df, agent, avg_reward, env, n_max=1000):
+#         patch_sequence = []
+#         leave_times = []
+
+#         for _, trial in subject_df.iterrows():
+#             patch_info = self.get_patch_info(trial['patch'])
+#             patch = Patch(
+#                 patch_info['initial_yield'], 
+#                 patch_info['decay_rate_mean']
+#             )
+#             patch.start_harvesting()
+            
+#             # Sample decay rate based on the agent's belief
+#             sampled_decay_rate = agent.sample_decay_rate(trial['patch'], env)
+#             patch.decay_rate = sampled_decay_rate  # Apply the sampled decay rate to the patch
+
+#             observed_rewards = []
+#             for t in range(1, n_max + 1):
+#                 reward = patch.get_reward()
+#                 observed_rewards.append(reward)
+                
+#                 # Update belief at the end of each batch
+#                 if t % self.batch_size == 0:
+#                     agent.update_belief(trial['patch'], env, observed_rewards)
+
+#                 if agent.decide_leave(reward, avg_reward):
+#                     # Final belief update upon leaving the patch
+#                     agent.update_belief(trial['patch'], env, observed_rewards)
+#                     patch_sequence.append(trial['patch'])
+#                     leave_times.append(t)
+#                     break
+
+#         return patch_sequence, leave_times
+
+#     def run_simulation(self, df_trials, subject_id=None):
+#         simulated_leave_times = []
+#         if subject_id is not None:
+#             subject_df = df_trials[df_trials['sub'] == subject_id]
+#             patch_sequence = []
+#             agent = Agent(
+#                 decay_rate_means=self.decay_rate_means,
+#                 decay_rate_stds=self.decay_rate_stds,
+#                 observation_var=self.observation_var
+#             )
+#             for env in subject_df['env'].unique():
+#                 avg_reward = self.avg_rewards[(subject_id, env)]
+#                 subject_env_df = subject_df[subject_df['env'] == env]
+#                 seq, leave_times = self.simulate_subject(subject_env_df, agent, avg_reward, env)
+#                 patch_sequence.extend(seq)
+#                 simulated_leave_times.extend(leave_times)
+#             return agent, patch_sequence, simulated_leave_times
+#         else:
+#             for subject in df_trials['sub'].unique():
+#                 subject_df = df_trials[df_trials['sub'] == subject]
+#                 for env in subject_df['env'].unique():
+#                     avg_reward = self.avg_rewards[(subject, env)]
+#                     agent = Agent(
+#                         decay_rate_means=self.decay_rate_means,
+#                         decay_rate_stds=self.decay_rate_stds,
+#                         observation_var=self.observation_var
+#                     )
+#                     subject_env_df = subject_df[subject_df['env'] == env]
+#                     _, leave_times = self.simulate_subject(subject_env_df, agent, avg_reward, env)
+#                     simulated_leave_times.extend(leave_times)
+#             df_trials['simulated_leaveT'] = simulated_leave_times
+#             df_trials['simulated_dmLeave'] = df_trials['simulated_leaveT'] - df_trials['meanLT']
+#             return df_trials
